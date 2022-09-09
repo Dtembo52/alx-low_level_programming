beb6cee15ece67ed384c3a5fe05d06f9bc5b1d16
Dtembo52 <dtembo52@gmail.com>
2022-09-08 19:07:59 -0700

    lets see

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints statement 
+ * main - entry point or start of the code
  *
- * Return : returns 0 
+ * Return: returns 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
