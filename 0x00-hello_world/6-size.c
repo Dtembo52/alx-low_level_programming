c4711952e0b87ad8078cc38e1fbf333539899448
Dtembo52 <dtembo52@gmail.com>
2022-09-08 19:59:41 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints sizes
+ * main - Prints the size of var types
  *
  * Return: outputs 0
  */
-int mian(void)
+int main(void)
 {
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float e;
-
+	
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
