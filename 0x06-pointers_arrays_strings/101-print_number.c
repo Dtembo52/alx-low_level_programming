0754723c238bb6557883332ba4cfe5fac1b9d552
Dtembo52 <dtembo52@gmail.com>
2022-09-22 19:13:38 -0700

    tasks 9 and 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,43 +1,18 @@
-include "main.h"
+#include "main.h"
 /**
- * rot13 - Encodes a string using rot13.
- *
- * Return: A pointer to the encoded string.
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
-char *rot13(char *str)
+void print_number(int n)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',                                                                            
-                             'G', 'H', 'I', 'J', 'K', 'L',                                                                            
-                             'M', 'N', 'O', 'P', 'Q', 'R',                                                                            
-                             'S', 'T', 'U', 'V', 'W', 'X',                                                                            
-                             'Y', 'Z', 'a', 'b', 'c', 'd',                                                                            
-                             'e', 'f', 'g', 'h', 'i', 'j',                                                                            
-                             'k', 'l', 'm', 'n', 'o', 'p',                                                                            
-                             'q', 'r', 's', 't', 'u', 'v',                                                                            
-                             'w', 'x', 'y', 'z'};                                                                                     
-        char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',                                                                            
-                             'T', 'U', 'V', 'W', 'X', 'Y',                                                                            
-                             'Z', 'A', 'B', 'C', 'D', 'E',                                                                            
-                             'F', 'G', 'H', 'I', 'J', 'K',                                                                            
-                             'L', 'M', 'n', 'o', 'p', 'q',                                                                            
-                             'r', 's', 't', 'u', 'v', 'w',                                                                            
-                             'x', 'y', 'z', 'a', 'b', 'c',                                                                            
-                             'd', 'e', 'f', 'g', 'h', 'i',                                                                            
-                             'j', 'k', 'l', 'm'};                                                                                     
-                                                                                                                                      
-        while (str[indx1])                                                                                                            
-        {                                                                                                                             
-                for (indx2 = 0; indx2 < 52; indx2++)                                                                                  
-                {                                                                                                                     
-                        if (str[indx1] == alphabet[indx2])                                                                            
-                        {                                                                                                             
-                                str[indx1] = rot13key[indx2];                                                                         
-                                break;                                                                                                
-                        }                                                                                                             
-                }                                                                                                                     
-                                                                                                                                      
-                indx1++;                                                                                                              
-        }                                                                                                                                      
-       return (str);                                                                                                                 
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
